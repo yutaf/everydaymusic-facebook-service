e0042fe3858fedc2abb703d7e57abe2b2c31ad2c
yutaf <yutafuji2008@gmail.com>
2015-07-22 11:52:41 +0900

    Add FacebooksRepository::fetchByFacebookUserId

--- models/FacebooksRepository.php
+++ models/FacebooksRepository.php
@@ -6,4 +6,19 @@ class FacebooksRepository extends DbRepository
     {
         $this->table_name = 'facebooks';
     }
+
+    /**
+     * fetch a record by facebook_user_id
+     *
+     * @param $facebook_user_id
+     * @return mixed
+     */
+    public function fetchByFacebookUserId($facebook_user_id)
+    {
+        $sql = <<<EOL
+SELECT * FROM {$this->table_name} WHERE facebook_user_id=:facebook_user_id
+;
+EOL;
+        return $this->fetch($sql, array(':facebook_user_id'=>$facebook_user_id));
+    }
 }
