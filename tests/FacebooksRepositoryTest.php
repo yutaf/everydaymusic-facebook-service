22cc72197256cc4fa07752708513858526b045c1
yutaf <yutafuji2008@gmail.com>
2015-07-22 11:53:29 +0900

    Create tests/FacebooksRepositoryTest.php

---
+++ tests/FacebooksRepositoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class FacebooksRepositoryTest extends PHPUnit_Framework_TestCase
+{
+
+    protected static $dbManager;
+
+    public static function setUpBeforeClass()
+    {
+        self::$dbManager = new DbManager();
+        self::$dbManager->connect($_ENV);
+    }
+
+    public function testFetchByFacebookUserId()
+    {
+        $facebook_user_id = '904535809601886';
+        $result = self::$dbManager->get('Facebooks')->fetchByFacebookUserId($facebook_user_id);
+    }
+}
