b037fbd47fbdd253c63307cfba6b678539288fff
yutaf <yutafuji2008@gmail.com>
2015-09-27 12:21:23 +0900

    Remove FacebooksRepository::fetchByFacebookUserId

--- models/FacebooksRepository.php
+++ models/FacebooksRepository.php
@@ -6,19 +6,4 @@ class FacebooksRepository extends Yutaf\DbRepository
     {
         $this->table_name = 'facebooks';
     }
-
-    /**
-     * fetch a record by facebook_user_id
-     *
-     * @param $facebook_user_id
-     * @return mixed
-     */
-    public function fetchByFacebookUserId($facebook_user_id)
-    {
-        $sql = <<<EOL
-SELECT * FROM {$this->table_name} WHERE facebook_user_id=:facebook_user_id
-;
-EOL;
-        return $this->fetch($sql, array(':facebook_user_id'=>$facebook_user_id));
-    }
 }
